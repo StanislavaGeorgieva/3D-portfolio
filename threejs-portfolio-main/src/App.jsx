974ba4619c6579e923a97f6e7ab57e87310e04c1
StanislavaGeorgieva <stanislava.georgieva.a58@learn.telerikacademy.com>
2024-09-20 21:51:19 +0300

    some-changes

--- threejs-portfolio-main/src/App.jsx
+++ threejs-portfolio-main/src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './sections/Hero.jsx';
 import Navbar from './sections/Navbar.jsx';
 import Project from './sections/Project.jsx';
 
+
 const App = () => {
   return (
     <main className="max-w-7xl mx-auto relative">
