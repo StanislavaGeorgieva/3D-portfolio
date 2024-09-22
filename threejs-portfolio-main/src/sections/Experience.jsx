4ec5fa3c53b1b3ed3fe6a785ac78909e9fbecf63
StanislavaGeorgieva <stanislava.georgieva.a58@learn.telerikacademy.com>
2024-09-22 17:10:23 +0300

    experience

--- threejs-portfolio-main/src/sections/Experience.jsx
+++ threejs-portfolio-main/src/sections/Experience.jsx
@@ -1,20 +1,38 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { workExperiences } from '../constants'
+import { OrbitControls, useAnimations } from '@react-three/drei'
+import CanvasLoader from '../components/CanvasLoader'
+import Developer from '../components/Developer'
+import { useState } from 'react'
+
+
+
 
 const Experience = () => {
+
+  const [animationsName, setAnimationsName] = useState('idle');
   return (
-    <section className='c-space my-20'>
+    <section className='c-space my-20 ' id="experience">
       <div className='w-full text-white-600'>
         <h3 className='head-text'>My experience</h3>
         <div className='work-container'>
           <div className='work-canvas'>
-            <Canvas></Canvas>
+          <Canvas>
+              <ambientLight intensity={3} />
+              <spotLight position={[10, 10, 10]} angle={0.3} penumbra={1} />
+              <directionalLight position={[10, 15, 15]} intensity={0.2} />
+              <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} />
+
+              <Suspense fallback={<CanvasLoader />}>
+                <Developer position-y={-3} scale={3} animationsName={animationsName}/>
+              </Suspense>
+            </Canvas>
           </div>
           <div className='work-content'>
             <div className='sm:py-10 py-5 sm:px-5 px-2.5'>
             {workExperiences.map(({id,name,pos,duration,title,animation,icon}) => (
-  <div key={id} className='work-content_container group'>
+  <div key={id} className='work-content_container group' onClick={()=>setAnimationsName(animation.toLowerCase())} onPointerOver={()=>setAnimationsName(animation.toLowerCase())} onPointerOut={()=>setAnimationsName('idle')}>
     <div className='flex flex-col h-full justify-start items-center py-2'>
       <div className='work-content_logo'>
         <img src={icon} alt="logo" className='w-full h-full' />
